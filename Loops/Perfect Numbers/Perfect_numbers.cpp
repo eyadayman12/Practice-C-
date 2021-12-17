f7a144da2a88b5edc602b9bd40f4f0ba321388d1
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2021-12-17 23:55:33 +0200

    Perfect Numbers Problem Solution

---
+++ Loops/Perfect Numbers/Perfect_numbers.cpp
@@ -0,0 +1,32 @@
+/**Write a program in C++ to find the perfect numbers between 1 and 500. Go to the editor
+The perfect numbers between 1 to 500 are:
+6
+28
+496**/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0, j = 0, iSum = 0;
+
+    for (int i=1; i<=500; i++)
+    {
+        for (int j=1; j<=500; j++)
+        {
+            if(i > j)
+            {
+                if (i % j == 0)
+                    iSum+=j;
+            }
+        }
+        if (iSum == i)
+            cout<<i<<"\n";
+        
+        j = 1;
+        iSum = 0;
+    }
+    return 0;
+}
