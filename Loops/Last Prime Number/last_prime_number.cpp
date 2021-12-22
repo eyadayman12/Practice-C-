4f967a752a2cb650fc6aa8aefbaedbc62c58e561
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2021-12-23 01:02:45 +0200

    Last Prime Number Problem Solution

---
+++ Loops/Last Prime Number/last_prime_number.cpp
@@ -0,0 +1,53 @@
+/** Write a program in C++ to find the last prime number occur before the entered number. 
+ * Sample Output:
+ * Input a number to find the last prime number occurs before the number: 50
+ * 47 is the last prime number before 50 
+ */
+
+///note:: Input Validation
+
+#include<iostream>
+#include<cmath>
+
+
+using namespace std;
+
+int main()
+{
+    d:int iNum, iPrime=0;
+    int iLastPrimeNumber=0;
+
+    while(true)
+    {
+        cout<<"Input a number to find the last prime number occurs before the number: ";
+        cin>>iNum;
+
+        if(cin.fail())
+        {
+            cout<<"\n\nError! Please Enter a valid number\n";
+            cin.clear();
+            cin.ignore(10000, '\n');
+        }
+        else
+        {
+            for(int i=1; i <=iNum; i++)
+            {
+                for (int j=2; j<=sqrt(i); j++)
+                {
+                    if (i % j == 0)
+                        iPrime++;
+                }
+                if (iPrime == 0 && i!=1)
+                    iLastPrimeNumber = i;
+
+                iPrime=0;
+
+            }
+            break;
+        }
+    }
+     cout<<iLastPrimeNumber;
+
+
+    return 0;
+}
