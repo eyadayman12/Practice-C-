6060166ff16cd8385ed902ec1af9813a5f5b9144
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-17 13:49:33 +0200

    New Year Problem Description and Solution

---
+++ Helwan icpc/Sheet 1/A. New Year.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Statment:
+ * How many hours do we have until New Year at M o'clock (24-hour notation) on 30th, December?
+ */
+
+///Solution:
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int m;
+    cin>>m;
+    cout<<48-m;
+    return 0;
+}
