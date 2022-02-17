1b88da000a2ea6139f2bbd2142a9b78dc3c66fd0
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-17 13:50:40 +0200

    Vasya The Hipster Problem Description and Solution

---
+++ Helwan icpc/Sheet 1/E. Vasya The Hipster.cpp
@@ -0,0 +1,21 @@
+/**
+ * Problem Statment:
+ * One day Vasya the Hipster decided to count how many socks he had. It turned out that he had a red socks and b blue socks.
+ * According to the latest fashion, hipsters should wear the socks of different colors: a red one on the left foot, a blue one on the right foot.
+ * Every day Vasya puts on new socks in the morning and throws them away before going to bed as he doesn't want to wash them.
+ * Vasya wonders, what is the maximum number of days when he can dress fashionable and wear different socks,
+ * and after that, for how many days he can then wear the same socks until he either runs out of socks or cannot make a single pair from the socks he's got.
+ */
+
+///Solution:
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<min(a,b)<<" "<<abs(a-b)/2;
+    return 0;
+}
