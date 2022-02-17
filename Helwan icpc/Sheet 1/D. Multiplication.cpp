46772b733d5f3a95577cf210231945dfaf3400b7
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-17 13:50:17 +0200

    Multiplication Problem Description and Solution

---
+++ Helwan icpc/Sheet 1/D. Multiplication.cpp
@@ -0,0 +1,17 @@
+/**
+ * Problem Statment:
+ * Compute AxB
+ */
+
+///Solution:
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short a,b;
+    cin>>a>>b;
+    cout<<a*b;
+    return 0; 
+}
