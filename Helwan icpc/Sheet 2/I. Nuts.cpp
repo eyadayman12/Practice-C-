a931dadc29be5f5d85f45fa2c44ea52990477eb4
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-20 01:27:38 +0200

    Nuts Problem Solution

---
+++ Helwan icpc/Sheet 2/I. Nuts.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n, count=0;
+    cin>>n;
+    int a[n];
+    for (int i=0; i<n; i++) {
+        cin>>a[i];
+        if (a[i] <= 10) count+=0;
+        else {
+            count+=(a[i] - 10);
+        }
+
+    }
+    cout<<count;
+
+    return 0;
+}
