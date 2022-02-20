b7f49f4e3b0dd2d0cf75cc7795a8fd6fd7e57360
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-20 02:00:45 +0200

    Nearest Interesting Number Problem Solution

---
+++ Codeforces/A/1183A. Nearest Interesting Number.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int a,sum=0,temp=0;
+    cin>>a;
+    for (int i=a; i<=10000; i++) {
+        int temp2 = i;
+        for (int j = 0; j<=10000; j++) {
+            temp=temp2%10;
+            sum+=temp;
+            temp2/=10;
+            if (temp2==0) break;
+        }
+        if (sum%4==0){
+            cout<<i;
+            break;
+        }
+        sum=0;
+    }
+
+    return 0;
+}
