618fd8d7eb57346825c633ce9dadedc722f6b88a
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-28 17:31:50 +0200

    Y Problem Solution

---
+++ Assuit icpc/Sheet 1/Y.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    if (((a%100)*(b%100)*(c%100)*(d%100)) % 100 < 10) cout<<0<<((a%100)*(b%100)*(c%100)*(d%100)) % 100;
+    else cout<<((a%100)*(b%100)*(c%100)*(d%100)) % 100;
+    return 0;
+}
