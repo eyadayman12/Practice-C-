020b5301ad3485134e08d28afe5ef985b1d31054
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-28 17:29:00 +0200

    G problem Solution

---
+++ Assuit icpc/Sheet 1/G.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin>>n;
+    long long sum=0;
+    sum=n*(n+1)/2; /// n = 3 , 3*(4)/2, 12/2=6, 10*(11)/2, 110/2 = 55
+    cout<<sum;
+    return 0;
+}
