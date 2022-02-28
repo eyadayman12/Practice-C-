f6dadf73af7c92f90bd701a0805f4107b2acb747
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-02-28 17:28:34 +0200

    E problem Solution

---
+++ Assuit icpc/Sheet 1/E.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    double r;
+    cin>>r;
+    const double pi = 3.141592653;
+    double area = pi * pow(r,2);
+    cout<<setprecision(11)<<area;
+    return 0;
+}
