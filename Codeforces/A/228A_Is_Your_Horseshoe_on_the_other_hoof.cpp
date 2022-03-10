5c41696fda0de8da1e9aacf2322a380ebd9a73e1
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-03-11 01:25:17 +0200

    228A Is Your Horseshoe on the other hoof Problem Solution

---
+++ Codeforces/A/228A_Is_Your_Horseshoe_on_the_other_hoof.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fr for(int i=0; i<4; i++)
+int main(){
+	int arr[4];
+	fr cin>>arr[i];
+	set<int>s;
+	fr s.insert(arr[i]);
+	cout<<4-s.size();
+}
