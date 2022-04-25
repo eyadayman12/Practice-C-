12065e43c75f7fd6eafaf4a3b0da08609527dfa2
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-04-25 02:31:45 +0200

    Add Odd or Subtract Even Problem Solution

---
+++ Codeforces/A/1311A_Add_Odd_or_Subtract_Even.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int a,b;
+		cin>>a>>b;
+		if (a==b) cout<<0<<endl;
+		else if (a<b && a%2==0 && b%2!=0) cout<<1<<endl;
+		else if (a>b && a%2==0 && b%2!=0) cout<<2<<endl;
+		else if (a>b && a%2!=0 && b%2==0) cout<<2<<endl;
+ 		else if (a<b && a%2!=0 && b%2==0) cout<<1<<endl;
+		else if (a<b && a%2==0 && b%2==0) cout<<2<<endl;
+		else if (a>b && a%2==0 && b%2==0) cout<<1<<endl;
+		else if (a<b && a%2!=0 && b%2!=0) cout<<2<<endl;
+		else if (a>b && a%2!=0 && b%2!=0) cout<<1<<endl;
+		
+		
+	}
+}
