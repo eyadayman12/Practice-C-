77fbb9fc2ef5bc40686df2ab52549be88726cb78
eyadayman12 <88289677+eyadayman12@users.noreply.github.com>
2022-06-15 02:46:31 +0200

    Fair Playoff Problem Solution

---
+++ Codeforces/A/1535A_Fair_Playoff.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int s1,s2,s3,s4;
+		cin>>s1>>s2>>s3>>s4;
+		int mx1 = max(s1,s2), mx2 = max(s3,s4);
+		int mn1 = min(s1,s2), mn2 = min(s3,s4);
+		if (mn1 > mx2 || mn2 > mx1) cout<<"NO"<<endl;
+		else cout<<"YES"<<endl;
+	}	
+}     
